12c167ba97a91b658e89cbd1729a2522f77a9de6
virginiaherrero <vikihpolo@gmail.com>
2020-02-27 09:16:04 +0100

    añadido_javascript_y_cambios_en_css

---
+++ script.js
@@ -0,0 +1,386 @@
+const Sobre_mi = `
+<div id="sobre_mi">
+<div id="imagen_descarga">
+    <img id="foto" src="imagenes/foto_2.png" alt="Foto de perfil Virginia Herrero Polo" />
+    
+    <a href="archivos/curriculum.pdf" target="_blank">
+    <div id="curriculum">
+
+            <img id="icono_descarga" src="imagenes/icono_descarga_amarillo.webp"
+                alt="Icono de descarga">
+            <h4 id="descarga">Descargar mi currículum en PDF</h4>
+        
+    </div>
+
+</a>
+</div>
+<div id="texto">
+    <div id="nombre">
+        <h1>VIRGINIA HERRERO POLO</h1>
+        <h2>Desarrolladora Web</h2>
+        <p id="mi_descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis officia
+            deleniti sit
+            aliquam
+            eveniet
+            praesentium dolore, ratione iusto consectetur ab sequi dignissimos doloribus eos quia iste
+            fugiat
+            fugit
+            dicta distinctio officiis pariatur. Sint iure nostrum voluptate veniam eveniet odit et
+            beatae
+            quos
+            perferendis natus quam voluptatibus, earum error molestiae ipsum dolor voluptas laboriosam
+            ducimus
+            nesciunt labore, temporibus assumenda ipsa aperiam?</p>
+    </div>
+    <div id="datos_personales">
+        <div class="enunciados">
+            <h4>Edad:</h4>
+            <h4>Dirección:</h4>
+            <h4>Email:</h4>
+        </div>
+        <div class="datos">
+            <h4>37</h4>
+            <h4>Alcorisa 57, 12º B, 28043 - MADRID</h4>
+            <h4><a href="mailto:vikihpolo@gmail.com">vikihpolo@gmail.com</a></h4>
+        </div>
+        <div class="enunciados">
+            <h4 class="enunciados">Teléfono:</h4>
+            <h4 class="enunciados">Linkedin:</h4>
+            <h4 class="enunciados">Nacionalidad:</h4>
+        </div>
+        <div class="datos">
+            <h4 class="datos">636723090</h4>
+            <a href="https://www.linkedin.com/in/virginia-herrero-6b691218a/" target="_blank">
+                <h4 class="datos">Mi Perfil</h4>
+                
+            </a>
+            <h4 class="datos">Española</h4>
+        </div>
+    </div>
+</div>
+</div>`;
+
+const Portfolio = ` <h1 class="titulo_principal">PORTFOLIO</h1>
+<div id="caja_general">
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 1</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto2.png" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 2</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto3.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 3</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto4.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 4</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto5.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 5</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto6.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 6</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto7.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 7</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto8.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 8</h3>
+
+        </div>
+    </a>
+    <a href="#">
+        <div class="caja_proyecto">
+            <img src="imagenes/proyecto9.jpg" alt="Imagen de Proyecto" class="imagen_proyecto">
+            <h3 class="titulo_proyecto">Proyecto Nº 9</h3>
+
+        </div>
+    </a>
+</div>`;
+
+const Formacion = `<h1 class="titulo_principal">FORMACIÓN</h1>
+<div id="caja_general">
+    <div class="estudio">   
+        <div class="caja_año">
+            <h3 class="año">2020</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Bootcamp Full Stack Developer - 560 horas</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Geekshubs Academy</p>
+        <p class="descripcion tabulacion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam doloribus ratione iure quaerat sunt nesciunt suscipit voluptatem? Molestias maxime eos quis, quam obcaecati fuga non facilis, nisi ipsam illo inventore?</p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2019</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Confección y Publicación de Páginas Web</p>
+        <p class="segundo titulo">Certificado de Profesionalidad - 520 horas</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Grupo Atrium</p>
+        <p class="descripcion tabulacion">Formación orientada como desarrolladora web, con conocimientos de Java, JSP, SQL, Html, CSS, Javascript y AJAX + <strong>80 horas de prácticas en la empresa Edyals</strong></p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2019</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Diseño Web - Itinerario Formativo (5 módulos)</p>
+        <p class="segundo titulo">140 horas</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Escuela de Leganés</p>
+        <p class="descripcion tabulacion">Itinerario formativo - Html, CSS, Javascript </p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2018</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Adiestramiento de base y educación canina</p>
+        <p class="segundo titulo">Certificado de profesionalidad - 380 horas</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Narub, Escuela y Residencia canina (Navalcarnero)</p>
+        <p class="descripcion tabulacion">Técnicas de adiestramiento de base, modificación de conductas no deseadas, cuidados higienicos y primeros auxilios aplicados a perros + Módulo de prácticas en un centro homologado (40 horas)</p>
+    </div>    
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2017</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Dirección y Coordinación de actividades de tiempo</p>
+        <p class="segundo titulo"> libre educativo infantil y juvenil</p>
+        <p class="segundo titulo">Certificado de profesionalidad- 410 horas</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Masercisa</p>
+        <p class="descripcion tabulacion">Planificación, organización, gestión y evaluación de proyectos educativos de tiempo libre infantil y juvenil. Procesos grupales y educativos. Técnicas y recursos de animación en actividades. Coordinación y dinamización del equipo de monitores.</p>
+    </div> 
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2002 - 2006</h3>
+        </div>
+        <div id="imagen_sonido">
+            <div class="imagen_sonido">
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Imagen</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">I.E.S. Valcárcel</p><br>
+            </div>
+            <div class="imagen_sonido">
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Sonido</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">I.E.S. Puerta Bonita</p>
+            </div>
+        </div>
+        <p class="descripcion tabulacion"><strong>Ciclos formativos de grado superior.</strong></p>
+        
+    </div>
+</div>`;
+
+const Experiencia =`<h1 class="titulo_principal">EXPERIENCIA LABORAL</h1>
+<div id="caja_general">
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2007 - 2017</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Gestor Telefónico</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Aqualia (Trainasa)</p>
+        <p class="descripcion tabulacion">Atención telefónica en el departamento de Aqualia Contact,
+            facilitando información, tramitando contratos de clientes y gestionando averías a nivel
+            nacional.</p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2006</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Teleoperadora</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Fénix Directo</p>
+        <p class="descripcion tabulacion">Contratación de pólizas de nueva producción, gestión y atención al
+            cliente.</p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2005</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Vendedora y Dependienta</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Petardos C.M.</p>
+        <p class="descripcion tabulacion">Dependienta en tienda durante la campaña de navidad.</p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2004 - 2005</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Monitora de Paintball</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Shootballs</p>
+        <p class="descripcion tabulacion">Coordinación de grupos, desarrollo de las actividades y/o eventos,
+            y preparación, limpieza y puesta a punto de los equipos.</p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2004 - 2005</h3>
+        </div>
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Operadora de Cámara y Edición</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Canal Norte T.V.</p>
+        <p class="descripcion tabulacion">Realización de grabaciones en plató, edición lineal y no-lineal,
+            encargada de continuidad y producción. (Prácticas)</p>
+    </div>
+    <div class="estudio">
+        <div class="caja_año">
+            <h3 class="año">2002 - 2003</h3>
+        </div>                    
+        <img src="imagenes/icono_titulo.png" alt="Icono de titulo" class="icono_titulo">
+        <p class="titulo">Auxiliar de Caja</p><br>
+        <img src="imagenes/icono_escuela.png" alt="Icono de escuela" class="icono_escuela">
+        <p class="escuela">Supermercados Alcosto</p>
+        <p class="descripcion tabulacion">Dependienta en caja, reponedora y atención al cliente.</p>
+    </div>
+</div>`;
+
+const Skills =`<h1 class="titulo_principal">SKILLS</h1>
+<div id="caja_general">
+    <div class="estudio">
+        <div class="caja_skills">
+            <h3 class="año tamaño">Conocimientos</h3>
+        </div>
+        <div class="contenido">
+            <div class="titulo_conocimientos">
+                <p class="padding">HTML</p>
+                <p class="padding">CSS</p>
+                <p class="padding">Javascript</p>
+                <p class="padding">Java</p>
+                <p class="padding">SQL</p>
+                <p class="padding">AJAX</p>
+                <p class="padding">Office</p>
+                <p class="padding">Photoshop, Premier</p>
+                <p class="padding">Mecanografía</p>
+                <p class="padding">Terapia asistida con animales</p>
+            </div>
+            <div class="estrellas">
+                <img src="imagenes/3-estrellas.png" alt="3 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/3-estrellas.png" alt="3 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/2-estrellas.png" alt="2 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/2-estrellas.png" alt="2 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/3-estrellas.png" alt="3 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/3-estrellas.png" alt="3 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/3-estrellas.png" alt="3 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/2-estrellas.png" alt="2 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/4-estrellas.png" alt="4 estrellas" class="icono_estrellas"><br>
+                <img src="imagenes/3-estrellas.png" alt="3 estrellas" class="icono_estrellas"><br>
+            </div>
+        </div>
+    </div>
+
+    <div class="estudio">
+        <div class="caja_skills">
+            <h3 class="año tamaño">Otros datos de interés</h3>
+        </div>
+        <div class="otros_datos">
+                <p class="padding2">Permiso de circulación clase B y vehículo propio</p>
+                <p class="padding2">10 años organizando y dinamizando grupos Scouts para niños y adolescentes</p>          
+                <p class="padding2">2 años organizando eventos y dinámicas de cohesión para adultos como voluntaria</p>
+                <p class="padding2">Curso de Manipuladora de Alimentos (2017)</p>
+                <p class="padding2">Conocimientos de Inglés - Nivel bajo</p>
+                <p class="padding2">Escalada y capoeira</p>
+                <p class="padding2">Juegos de mesa y estrategia</p>
+                <p class="padding2">Disponibilidad a jornada completa</p>
+                <p class="padding2">Posibilidad de incorporación inmediata</p>                    
+        </div>
+    </div>
+</div>`;
+
+const Contacto =`<h1 class="titulo_principal">CONTÁCTAME:</h1>
+<p>&nbsp;</p>
+<form action="#" method="post">
+    <input name="name" placeholder="Nombre" type="text" required>
+    <input name="empresa" placeholder="Empresa" type="text" required>   
+    <input name="email" placeholder="E-mail" type="email" required>
+    <input name="phone" placeholder="Teléfono (9 dígitos)" type="text" pattern="^[9|8|7|6]\d{8}$" required>
+    <textarea name="message" placeholder="Mensaje" required></textarea>
+
+    <input type="reset" value="Borrar">
+    <input type="submit" value="Enviar">
+
+</form>`;
+
+const main = document.querySelector('main')
+window.addEventListener('load', () => {
+    if (window.location.hash === '#portfolio') {
+        main.innerHTML = Portfolio
+    } else if (window.location.hash === '#estudios') {
+        main.innerHTML = Formacion
+    }else if (window.location.hash === '#experiencia') {
+        main.innerHTML = Experiencia
+    }else if (window.location.hash === '#skills') {
+        main.innerHTML = Skills
+    }else if (window.location.hash === '#contacto') {
+        main.innerHTML = Contacto
+
+    }else{
+        main.innerHTML =Sobre_mi
+    }
+
+})
+document.querySelector('.index').addEventListener('click', () => {
+    main.innerHTML = Sobre_mi;
+    history.pushState({}, '', '#');
+})
+document.querySelector('.portfolio').addEventListener('click', () => {
+    main.innerHTML = Portfolio;
+    history.pushState({}, '', '#portfolio');
+})
+document.querySelector('.estudios').addEventListener('click', () => {
+    main.innerHTML = Formacion;
+    history.pushState({}, '', '#estudios');
+})
+document.querySelector('.experiencia').addEventListener('click', () => {
+    main.innerHTML = Experiencia;
+    history.pushState({}, '', '#experiencia');
+})
+document.querySelector('.skills').addEventListener('click', () => {
+    main.innerHTML = Skills;
+    history.pushState({}, '', '#skills');
+})
+document.querySelector('.contacto').addEventListener('click', () => {
+    main.innerHTML = Contacto;
+    history.pushState({}, '', '#contacto');
+})
